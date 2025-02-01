522ffa3a0a759c75a825c19c8e211daae7eda48b
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-02-01 16:54:26 -0300

    Trabalhando com múltiplos Datasources

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,11 +1,11 @@
 output "bucket_domain_name" {
-  value       = aws_s3_bucket.bucket.bucket_domain_name
+  value       = data.aws_s3_bucket.bucket.bucket_domain_name
   sensitive   = false
   description = "Nome de domínio do bucket S3"
 }
 
 output "bucket_id" {
-  value       = aws_s3_bucket.bucket.id
+  value       = data.aws_s3_bucket.bucket.id
   sensitive   = false
   description = "ID de domínio do bucket S3"
 }
