77c4e141297f721eac48e43b1b7860a945044ce8
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-02-01 16:33:46 -0300

    Criando recursos do novo Módulo

--- resources.tf
+++ resources.tf
@@ -1,13 +1,29 @@
 module "s3" {
   source         = "./modules/s3"
   s3_bucket_name = "rodrigofontes-iac"
+  s3_tags = {
+    Iac = true
+  }
 }
 
 module "cloudfront" {
   source             = "./modules/cloudfront"
   origin_id          = module.s3.bucket_id
   bucket_domain_name = module.s3.bucket_domain_name
+  cdn_price_class    = "PriceClass_200"
+  cdn_tags = {
+    Iac = true
+  }
   depends_on = [
     module.s3
   ]
 }
+
+# module "sqs" {
+#   source     = "terraform-aws-modules/sqs/aws"
+#   name       = "rodrigofontes-sqs"
+#   create_dlq = true
+#   tags = {
+#     Iac = true
+#   }
+# }
